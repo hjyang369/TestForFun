a5a0b60cf48348dad758ced5be2b7f9d9eb44d92
hjyang369 <ghlwls6@gmail.com>
2023-10-05 00:56:39 +0900

    Next + TS + SCSS 초기세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.scss';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <p>메인 index 파일입니다.</p>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -110,5 +111,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
